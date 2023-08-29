96eccaf0aa06d6a39d91594a2302f5a29afe1cd1
jbbrejon <jeanbaptiste.brejon.pro@gmail.com>
2023-08-29 15:08:15 +0200

    :construction: Change class when link is active

--- app/_layouts/nav.tsx
+++ app/_layouts/nav.tsx
@@ -1,11 +1,39 @@
+'use client'
+
 import Link from "next/link"
+import { usePathname } from 'next/navigation'
 
 export default function Nav() {
+    const pathname = usePathname()
+    const navLinks = [
+        {
+            "href": "/",
+            "name": "Home"
+        },
+        {
+            "href": "/about",
+            "name": "About"
+        }
+    ]
     return (
         <nav>
             <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
+                {navLinks.map((link) => {
+                    const isActive = pathname === link.href
+                    return (
+                        <li>
+                            <Link
+                                key={link.name}
+                                href={link.href}
+                                className={isActive ? "text-blue-500 underline" : "text-black"}
+                            >
+                                {link.name}
+                            </Link>
+                        </li>
+                    )
+                }
+                )
+                }
             </ul>
         </nav>
     )
